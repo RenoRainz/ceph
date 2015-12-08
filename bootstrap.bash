67212f6e4aba88e799515b52a6bb269955e1f2a9
RenoRainz <rainzreno@gmail.com>
2015-12-08 01:31:56 +0100

    Update bootstrap.bash

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,8 +1,18 @@
-sudo useradd -d /home/ceph -m ceph
-sudo passwd cephuser
-sudo echo "cephuser ALL = (root) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/cephuser
-sudo chmod 0440 /etc/sudoers.d/cephuser
-sudo apt-get install ntp ntpdate ntp-doc
-sudo /etc/init.d/apparmor stop
-sudo /etc/init.d/apparmor teardown
-sudo apt-get remove apparmor
+#/bin/bash
+useradd -d /home/ceph -m ceph
+echo "ceph:ceph" | chpasswd
+echo "cephuser ALL = (root) NOPASSWD:ALL" | tee /etc/sudoers.d/cephuser
+chmod 0440 /etc/sudoers.d/cephuser
+
+apt-get install ntp ntpdate ntp-doc
+/etc/init.d/apparmor stop
+/etc/init.d/apparmor teardown
+apt-get remove apparmor
+
+parted -s /dev/sdb mklabel gpt
+parted -s /dev/sdb mkpart primary 0% 50%
+parted -s /dev/sdb mkpart primary 51% 100%
+parted -s /dev/sdc mklabel gpt
+parted -s /dev/sdc mkpart primary xfs 0% 100%
+parted -s /dev/sdd mklabel gpt
+parted -s /dev/sdd mkpart primary xfs 0% 100%
