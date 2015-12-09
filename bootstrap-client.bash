83ac99abf5efdd22194f2570820384ba47730cf5
RenoRainz <rainzreno@gmail.com>
2015-12-09 01:26:05 +0100

    Init bootstrap file for ceph client.

---
+++ bootstrap-client.bash
@@ -0,0 +1,17 @@
+#/bin/bash
+
+useradd -d /home/ceph -s /bin/bash -m ceph
+echo "ceph:ceph" | chpasswd
+echo "ceph ALL = (root) NOPASSWD:ALL" | tee /etc/sudoers.d/ceph
+chmod 0440 /etc/sudoers.d/ceph
+
+mkdir /home/ceph/.ssh
+chmod 700 /home/ceph/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoc+s56EuX7DXURRCbGiezaynKRS8MRWogkSMvxF4Z6M+ebRGJTFkoGxlEGeBH5Hf+yTfM/QIywIj/v5I8Pi7QNZDE2ttIucnloyFnMHHZVTF7al/JjLqYAaerYpQ9yCuBoFjEKdE4m04mCSrg3Tsk7t6eWBniLqTdCDr4ZiuAvf4fSPHca+3Jp8p2all3Lmny/doYxDhpucv3rQ3UAx+IH07rLJU4eJSMWqwGRz0NZY1qCWByl857dP9EFMiXU1g8OtDnAZo7V107V6DCc6HVBdiGLz5Ydtrbq5B2SHxgzKTgJEmfehcZXqNeos0chf4056ocCGg1Cvk/xWfo8mlZ cephuser@ceph-admin" > /home/ceph/.ssh/authorized_keys
+chmod 600 /home/ceph/.ssh/authorized_keys
+chown -R ceph:ceph /home/ceph/
+
+apt-get update && apt-get install -y ntp ntpdate ntp-doc xfsprogs
+/etc/init.d/apparmor stop
+/etc/init.d/apparmor teardown
+apt-get remove -y apparmor
