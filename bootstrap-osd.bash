c2b3e03ac6b2be0755930e64a93cc22a4f7420af
RenoRainz <rainzreno@gmail.com>
2015-12-09 23:35:08 +0100

    Rename bootstrap.bash bootstrap-osd.bash

---
+++ bootstrap-osd.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+useradd -d /home/ceph -s /bin/bash -m ceph
+echo "ceph:ceph" | chpasswd
+echo "ceph ALL = (root) NOPASSWD:ALL" | tee /etc/sudoers.d/ceph
+chmod 0440 /etc/sudoers.d/ceph
+
+mkdir /home/ceph/.ssh
+chmod 700 /home/ceph/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoc+s56EuX7DXURRCbGiezaynKRS8MRWogkSMvxF4Z6M+ebRGJTFkoGxlEGeBH5Hf+yTfM/QIywIj/v5I8Pi7QNZDE2ttIucnloyFnMHHZVTF7al/JjLqYAaerYpQ9yCuBoFjEKdE4m04mCSrg3Tsk7t6eWBniLqTdCDr4ZiuAvf4fSPHca+3Jp8p2all3Lmny/doYxDhpucv3rQ3UAx+IH07rLJU4eJSMWqwGRz0NZY1qCWByl857dP9EFMiXU1g8OtDnAZo7V107V6DCc6HVBdiGLz5Ydtrbq5B2SHxgzKTgJEmfehcZXqNeos0chf4056ocCGg1Cvk/xWfo8mlZ cephuser@ceph-admin" > /home/ceph/.ssh/authorized_keys
+chmod 600 /home/ceph/.ssh/authorized_keys
+chown -R ceph:ceph /home/ceph/
+
+apt-get update && apt-get install -y ntp ntpdate ntp-doc xfsprogs
+/etc/init.d/apparmor stop
+/etc/init.d/apparmor teardown
+apt-get remove -y apparmor
+
+parted -s /dev/sdb mklabel gpt
+parted -s /dev/sdb mkpart primary 0% 50%
+parted -s /dev/sdb mkpart primary 51% 100%
+
+parted -s /dev/sdc mklabel gpt
+parted -s /dev/sdc mkpart primary xfs 0% 100%
+mkfs.xfs /dev/sdc1
+
+parted -s /dev/sdd mklabel gpt
+parted -s /dev/sdd mkpart primary xfs 0% 100%
+mkfs.xfs /dev/sdd1
+
+mkdir -p /var/local/osd0
+mkdir -p /var/local/osd1
+echo "/dev/sdc1	/var/local/osd0	xfs	defaults	0 0" >> /etc/fstab
+echo "/dev/sdd1	/var/local/osd1	xfs	defaults	0 0" >> /etc/fstab
+mount /var/local/osd0
+mount /var/local/osd1
